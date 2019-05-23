0d5c62ac10621b280ab07ab4989f491a937b7106
rurrea <rurrea@homeaway.com>
2019-05-23 16:10:13 +0200

    adds date format func to template

--- main.go
+++ main.go
@@ -75,6 +75,9 @@ func main() {
 				inboxCache.DeleteExpired()
 				return inboxCache.ItemCount()
 			},
+			"DateFormat": func(t time.Time) string {
+				return t.Format("2006-01-02 15:04:05")
+			},
 		}},
 		Extensions: []string{".gohtml", ".tmpl", ".html"},
 	}))
